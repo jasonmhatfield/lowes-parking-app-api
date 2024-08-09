360dd492fcea9e1e32643d2e656b577af495cea0
Jason Hatfield <jasonmhatfield@pm.me>
2024-08-09 08:40:39 -0400

    Update User to AddUser

--- src/main/java/com/lowes/lowesparkingappapi/repository/UserRepository.java
+++ src/main/java/com/lowes/lowesparkingappapi/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.lowes.lowesparkingappapi.repository;
 
-import com.lowes.lowesparkingappapi.model.User;
+import com.lowes.lowesparkingappapi.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<AppUser, Long> {}
--- src/main/java/com/lowes/lowesparkingappapi/service/AppUserService.java
+++ src/main/java/com/lowes/lowesparkingappapi/service/AppUserService.java
@@ -1,6 +1,6 @@
 package com.lowes.lowesparkingappapi.service;
 
-import com.lowes.lowesparkingappapi.model.User;
+import com.lowes.lowesparkingappapi.model.AppUser;
 import com.lowes.lowesparkingappapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,19 +8,23 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+public class AppUserService {
+    private final UserRepository userRepository;
 
-    public List<User> getAllUsers() {
+    @Autowired
+    public AppUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<AppUser> getAllUsers() {
         return userRepository.findAll();
     }
 
-    public User getUserById(Long id) {
+    public AppUser getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
     }
 
-    public User saveUser(User user) {
+    public AppUser saveUser(AppUser user) {
         return userRepository.save(user);
     }
 
